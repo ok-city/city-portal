55da39967eed86ebb9adeb335ab31c7f82802d77
Marc Bacvanski <marc.bacvanski@gmail.com>
2017-02-19 00:27:55 -0800

    Sentiments on table

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
 let map;
-function initMap() {
+function initMap() { // Called by async request to Google
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
@@ -47,9 +47,9 @@ function getReportsInArea(latitude, longitude) {
   let getReportsURL = '/getReports/?lat=' + latitude + '&lon=' + longitude + '&radius=' + 500000;
   httpGetAsync(getReportsURL).then((data) => {
     let reportSentiments = [];
-    reports.push(data);
     let jsonData = JSON.parse(data);
     jsonData.forEach((report) => {
+      reports.push(report);
       placeReportOnMap(report); // whoosh
       let url = '/getSentiment/?text=' + report.transcript;
       let sentimentHere = httpGetAsync(url);
@@ -58,34 +58,53 @@ function getReportsInArea(latitude, longitude) {
     return Promise.all(reportSentiments);
   }).then((sentiments) => {
     sentiments.forEach((sentiment, i) => {
-      placeSentimentOnMap(sentiment, reports[i]);
+      $('#tableOfReports').append(generateHtmlForTableRow(reports[i], sentiment));
+      // TODO Add to list of sentiments to retrieve later
     });
   }).catch((statusCode) => {
     console.error('error code: ' + statusCode);
   });
 }
 
-function placeSentimentOnMap(sentiment, report) {
-
-  let theActualReport = JSON.parse(report)[0];
-  let opacity = Math.min(Math.min(sentiment.magnitude, 0), 1);
-  // let redComponent =
-
-  console.log('theActualReport = ' + theActualReport);
-  console.log('coordinates = ' + theActualReport.location.coordinates);
-
-  let sentimentCircle = new google.maps.Circle({
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#ff7663',
-    fillOpacity: 0.35,
-    map: map,
-    center: {lat: theActualReport.location.coordinates[1],
-      lng: theActualReport.location.coordinates[0]},
-    radius: 10
-  });
+function generateHtmlForTableRow(report, sentiment) {
+  let td = '';
+  let sentimentJSON = JSON.parse(sentiment);
+  console.log('sentiment = ' + sentiment);
+  if (sentimentJSON.score >= 0.0) {
+    td = '<td><img src="/public/images/caret-up.svg" class="sentimentArrow"></td>';
+  } else if (sentimentJSON.score <= -0.0) {
+    td = '<td><img src="/public/images/caret-down.svg" class="sentimentArrow"></td>';
+  } else {
+    td = '<td></td>'
+  }
+  let tr = '<tr><td>' + report.transcript + '</td>' + td + '</tr>';
+  console.log('generated tr ' + tr);
+  return tr;
 }
+
+/*
+ function placeSentimentOnMap(sentiment, report) {
+
+ let theActualReport = JSON.parse(report)[0];
+ let opacity = Math.min(Math.min(sentiment.magnitude, 0), 1);
+ // let redComponent =
+
+ console.log('theActualReport = ' + theActualReport);
+ console.log('coordinates = ' + theActualReport.location.coordinates);
+
+ let sentimentCircle = new google.maps.Circle({
+ strokeColor: '#FF0000',
+ strokeOpacity: 0.8,
+ strokeWeight: 2,
+ fillColor: '#ff7663',
+ fillOpacity: 0.35,
+ map: map,
+ center: {lat: theActualReport.location.coordinates[1],
+ lng: theActualReport.location.coordinates[0]},
+ radius: 10
+ });
+ }
+ */
 
 function placeReportOnMap(report) {
   let reportLocation = new google.maps.LatLng(
@@ -112,11 +131,10 @@ function placeReportOnMap(report) {
   });
 
   marker.addListener('click', function () {
-    openNav();
+    infowindow.open(map, marker);
   });
 
 }
-
 
 function httpGetAsync(theUrl) {
   console.log('getting async! ');
